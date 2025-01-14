45e5dc27af26fa151ecb87c7f83c71d049ddbc35
Surodeep <sheth.surodeep@gmail.com>
2025-01-14 16:24:53 +0100

    .

---
+++ run_sixte.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export SIMPUT=/home/suro/SIXTE/installation
+export SIXTE=/home/suro/SIXTE/installation
+
+esass
+source /home/suro/heasoft-6.34/hea_init.sh
+source ${SIXTE}/bin/sixte-install.sh
